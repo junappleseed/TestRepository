6b6a9a60a4b7e8ecd7f240586e4b972d6e66cdcc
junappleseed <junappleseed@gmail.com>
2015-09-06 21:42:31 +0900

    Initial Commit

---
+++ TestRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestRepository
+//
+//  Created by Jun on 2015/09/06.
+//  Copyright (c) 2015年 junappleseed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
