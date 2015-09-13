5038e9cba4d41894c6770096dff54a8a62322cc2
junappleseed <junappleseed@gmail.com>
2015-09-13 17:53:32 +0900

    ボタンとテキストボックスを追加。

--- TestRepository/ViewController.swift
+++ TestRepository/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func changeLabel(sender: AnyObject) {
+        myLabel.text = "Hello World!"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
